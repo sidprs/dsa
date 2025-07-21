aee4b076114540e7ade0e1ddc2f793b109bdf9dd
sid <prasadsid@CHA-BZ2XMW3.ad.garmin.com>
2025-07-21 08:33:03 -0700

    added graph

--- alg/graph.cpp
+++ alg/graph.cpp
@@ -2,8 +2,10 @@
 #include <queue>
 #include <vector>
 #include <unordered_map>
-
-
+#include <queue>
+#include <string>
+#include <sstream>
+#include <iterator>
 // 
 //
 //
@@ -13,8 +15,9 @@
 template<typename T>
 struct vertex{
   int index;
-  int weight;
-  T value;
+  T weight;
+  int value;
+  bool visited;
 };
 
 template<typename T>
@@ -31,10 +34,11 @@ struct HEAP{
     // other fields as you see fit
 };
 
+struct Queue{
 
 
 
-
+};
 
 
 template<typename T>
@@ -42,13 +46,13 @@ class Graph{
   public:
     explicit Graph(int cap) :
       capacity_{cap},
-      KeySize_{0},
+      KeySize_{0}
       //vertex_list_(new vertex[cap])
       {}
       
     std::unordered_map <int,std::vector<vertex<T>*>> map; 
     
-    void push(int, T, int);
+    void push(int, int , T);
     //        ind  val weight
     void PrintMap();
    
@@ -69,8 +73,9 @@ class Graph{
 
 };
 
+// makes it adj automatically because goated
 template <class T>
-void Graph<T>::push(int index, T vertex_value, int weight){
+void Graph<T>::push(int index, int vertex_value, T weight){
   vertex<T> *new_vertex = new vertex<T>;
   new_vertex->index = index;
   new_vertex->value = vertex_value;
@@ -78,37 +83,74 @@ void Graph<T>::push(int index, T vertex_value, int weight){
   if(map[index].size() < 0){
     this->KeySize_++;
   }
+  
+  vertex<T> *comp =  new vertex<T>;
+  comp->index = vertex_value;
+  comp->value = index;
+
   map[index].push_back(new_vertex); 
+  map[vertex_value].push_back(comp);
 }
 
-template <class T>
-void Graph<T>::PrintMap()
-{
-    for (const auto& [key, vec] : map)           // C++17 structured binding
-    {
+template<typename T>
+void Graph<T>::PrintMap() {
+    for (const auto& [key, vec] : map) {
         std::cout << "key " << key << " : ";
-        for (const auto* v : vec) {              // vec holds vertex<T>* pointers
-            std::cout << "[ " << v->value << " , " << v->weight << " ] " ;        
+
+        for (std::size_t i = 0; i < vec.size(); ++i) {
+            std::cout << vec[i]->value;          // print the stored value
+            if (i + 1 < vec.size()) {
+                std::cout << ", ";               // comma only between items
+            }
         }
         std::cout << '\n';
     }
 }
 
 template<class T>
+void Graph<T>::PrintBFS()
+{
+ 
+  // 0 : 2, 3
+  // 1 : 1, 2, 3
+  int size = map.size();
+  int source = 0;
+  std::queue<int> q;
+  std::vector<bool> visited(size, false);
+ 
+  std::vector<int> result(size);
+
+  q.push(map[source]->index);
+  visited[map[source]->index] = true;
+  
+  while(!q.empty()){
+      int curr = q.pop();
+      visited[curr] = true;
+
+      for(auto &v : map[curr]){
+        if(!visited[v]){
+          visited[v] = true;
+          q.push(v);
+        }
+
+      }
+
+
+  }
+  for(auto& v : result){
+    std::cout << v << std::endl;
+  }
+
+}
+  
+template<class T>
 void Graph<T>::PrintDFS()
 {
 
-
-}
-
-
-template<class T>
-void Graph<T>::PrintBFS()
-{
+  
 
 
 }
-
 
 template<class T>
 void Graph<T>::PrintDKstra()
@@ -138,17 +180,21 @@ void Graph<T>::FindAndRemoveCycles()
 int main (int argc, char *argv[]) {
   std::cout << "[ node, weight ]" << std::endl;
   Graph<int> g (3);  
-  g.push(0, 3, 2);
+  g.push(0, 3, 1);
   g.push(0, 2, 1);
-  g.push(1, 1, 1);
+  g.push(1, 5, 1);
   g.push(1, 2, 1);
   g.push(1, 3, 1);
   //g.push(1);
 
 
+  // 0 -> 3, 2
+  // 1 -> 5, 2, 3
+  // 2 -> 0, 1
+  // 3 -> 0, 1
+  // 5 -> 1
   g.PrintMap();
-
-  std::cout <<g.KeySize_ << std::endl;
+  g.PrintBFS();
 
   return 0;
 }
