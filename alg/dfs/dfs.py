06fad00688341ebb01c5a5c6011413d7e5b5344d
sid <prasadsid@CHA-BZ2XMW3.ad.garmin.com>
2025-07-30 14:14:00 -0700

    g changes

---
+++ alg/dfs/dfs.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Graph():
+    def __init__(self, , , field3):
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+
+
+    def bfs( index: int ) -> int:
+        return 0
+
+
+def main() -> None:
+    return None
+
+
+
+if __name__ == '__main__':
+    main()
